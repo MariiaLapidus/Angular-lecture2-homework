8b9ccef255e7256a2276f6db9e551bd16c3356db
mmvli <mmvlivo@gmail.com>
2020-04-07 20:03:26 +0300

    services

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {PostModel} from '../models/PostModel';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) {
+    // this.http.get<PostModel[]>('https://jsonplaceholder.typicode.com/posts').subscribe(value => {
+    //   console.log(value);
+    //   this.posts = value;
+
+    // });
+  }
+  getPosts(): Observable<PostModel[]> {
+    return this.http.get<PostModel[]>('https://jsonplaceholder.typicode.com/posts');
+  }
+  getPost(id: number) {
+    return this.http.get<PostModel[]>('https://jsonplaceholder.typicode.com/posts/${id}');
+  }
+}
